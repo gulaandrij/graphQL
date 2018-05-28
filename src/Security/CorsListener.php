3a7350ac77c692f1fbcb2c2ed50d2c01687efc73
gulaandrij <gyla.andrij@gmail.com>
2018-05-28 16:59:17 +0300

    wip

--- src/Security/CorsListener.php
+++ src/Security/CorsListener.php
@@ -18,7 +18,7 @@ class CorsListener
         $responseHeaders = $event->getResponse()->headers;
 
         $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept, credentials');
-        $responseHeaders->set('Access-Control-Allow-Origin', 'http://localhost:8080');
+        $responseHeaders->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         $responseHeaders->set('Access-Control-Allow-Credentials', 'true');
         $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
     }
